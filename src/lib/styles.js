9490d844079ed0efd8a684afa86d42987d5e6e89
Dave Houston <root@crankyadmin.net>
2020-12-31 12:33:54 +0000

    Update styles.js
    Removed background colour from the default style as it is currently impossible to change the background of the container. 
    
    With `backgroundColor` removed it will just inherit its parent colour.

--- src/lib/styles.js
+++ src/lib/styles.js
@@ -1,11 +1,9 @@
 export default {
   activeContainer: {
     border: 'none',
-    backgroundColor: '#ffffff',
   },
   inActiveContainer: {
     border: 'none',
-    backgroundColor: '#ffffff',
     outline: 0,
   },
 };
