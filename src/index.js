6b189702a19917d8f639b3922f5ec905e18ac77a
Fedor Yakubovich <f.yakubovich@gmail.com>
2022-04-21 01:16:36 +0300

    Update dependencies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import ReactStarsExamples from './examples';
 
-ReactDOM.render(<ReactStarsExamples />, document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ReactStarsExamples />
+  </React.StrictMode>,
+  root,
+);
