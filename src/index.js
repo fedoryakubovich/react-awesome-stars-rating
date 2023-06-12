da664507c1a9da1795346ce615f6d929cedd28ec
Fedor Yakubovich <f.yakubovich@gmail.com>
2023-06-12 17:07:53 +0300

    Minor bug fixes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactStarsExamples from './examples';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <ReactStarsExamples />
-  </React.StrictMode>,
-);
+ReactDOM.render(<ReactStarsExamples />, document.getElementById('root'));
